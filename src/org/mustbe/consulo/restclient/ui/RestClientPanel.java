76762c71cffe716f8336d47d7d79b0ec0ad9f3a9
VISTALL <vistall.valeriy@gmail.com>
2015-03-25 17:20:35 +0300

    ui update

--- src/org/mustbe/consulo/restclient/ui/RestClientPanel.java
+++ src/org/mustbe/consulo/restclient/ui/RestClientPanel.java
@@ -17,6 +17,8 @@
 package org.mustbe.consulo.restclient.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Insets;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,11 +28,11 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 
@@ -71,10 +73,15 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.ColoredListCellRendererWrapper;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.JBSplitter;
+import com.intellij.ui.OnePixelSplitter;
+import com.intellij.ui.ScrollPaneFactory;
+import com.intellij.ui.SideBorder;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.TabbedPaneWrapper;
 import com.intellij.ui.TextFieldWithAutoCompletion;
-import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.table.JBTable;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.util.ui.ColumnInfo;
 import com.intellij.util.ui.ListTableModel;
 import com.intellij.util.ui.UIUtil;
@@ -94,9 +101,9 @@ import lombok.val;
  * @since 20.11.13.
  */
 @ProjectService
-public class RestClientPanel extends Ref<Project>  implements Disposable
+public class RestClientPanel extends Ref<Project> implements Disposable
 {
-	private static final String[] ourSupportedMethods = new String[] {
+	private static final String[] ourSupportedMethods = new String[]{
 			"GET",
 			"DELETE",
 			"HEAD",
@@ -105,15 +112,13 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 			//"PUT",
 			//"PATCH"
 	};
-	private JComboBox myMethodComboBox;
-	private JPanel myRootPanel;
-	private JTabbedPane myTabPanel;
-	private JPanel myResponseTab;
-	private JBSplitter myResponseSplitter;
+	private final JComboBox myMethodComboBox;
+	private final JPanel myRootPanel;
+	private final JPanel myResponseTab;
 
-	private TextFieldWithAutoCompletionWithEnter myUrlTextField;
-	private JLabel myResultLabel;
-	private JComboBox myHttpVersionBox;
+	private final TextFieldWithAutoCompletionWithEnter myUrlTextField;
+	private final JLabel myResultLabel;
+	private final JComboBox myHttpVersionBox;
 
 	private List<Pair<String, String>> myHeaders = new ArrayList<Pair<String, String>>();
 
@@ -122,6 +127,64 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 	public RestClientPanel(final Project project)
 	{
 		super(project);
+
+		myRootPanel = new JPanel();
+		myRootPanel.setLayout(new BorderLayout(0, 0));
+		myRootPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5), null));
+		final JPanel panel1 = new JPanel();
+		panel1.setLayout(new BorderLayout(0, 0));
+		myRootPanel.add(panel1, BorderLayout.CENTER);
+		final JPanel panel2 = new JPanel();
+		panel2.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+		panel1.add(panel2, BorderLayout.NORTH);
+		myMethodComboBox = new JComboBox();
+		panel2.add(myMethodComboBox, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL,
+				GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+		myUrlTextField = new TextFieldWithAutoCompletionWithEnter(get(), new TextFieldWithAutoCompletion.StringsCompletionProvider(null, null)
+		{
+			@NotNull
+			@Override
+			public Collection<String> getItems(String prefix, boolean cached, CompletionParameters parameters)
+			{
+				if(prefix == null)
+				{
+					return Collections.emptyList();
+				}
+
+				List<String> list = new ArrayList<String>();
+				for(RequestBean o : RestClientHistoryManager.getInstance(get()).getRequests().values())
+				{
+					list.add(o.getUrl().toString());
+				}
+				return list;
+			}
+		}, false, null);
+		myUrlTextField.setPlaceholder("URL");
+		panel2.add(myUrlTextField, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+				GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK |
+				GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+		myHttpVersionBox = new JComboBox();
+		panel2.add(myHttpVersionBox, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL,
+				GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+
+		final TabbedPaneWrapper tabbedPaneWrapper = new TabbedPaneWrapper(this);
+		panel1.add(tabbedPaneWrapper.getComponent(), BorderLayout.CENTER);
+		myResponseTab = new JPanel();
+		myResponseTab.setLayout(new BorderLayout(0, 0));
+		tabbedPaneWrapper.addTab("Response", myResponseTab);
+		final JPanel panel3 = new JPanel();
+		panel3.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+		myResponseTab.add(panel3, BorderLayout.CENTER);
+		JBSplitter responseSplitter = new OnePixelSplitter();
+		responseSplitter.setLayout(new BorderLayout(0, 0));
+		panel3.add(responseSplitter, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+				GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK |
+				GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+		myResultLabel = new JLabel();
+		myResultLabel.setEnabled(true);
+		myResultLabel.setText("");
+		panel3.add(myResultLabel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_SOUTHWEST, GridConstraints.FILL_HORIZONTAL,
+				GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
 
 		myMethodComboBox.setRenderer(new ColoredListCellRendererWrapper<String>()
 		{
@@ -141,7 +204,8 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 			myMethodComboBox.addItem(httpMethod);
 		}
 
-		myHttpVersionBox.setRenderer(new ColoredListCellRendererWrapper<Protocol>() {
+		myHttpVersionBox.setRenderer(new ColoredListCellRendererWrapper<Protocol>()
+		{
 			@Override
 			protected void doCustomize(JList list, Protocol value, int index, boolean selected, boolean hasFocus)
 			{
@@ -161,8 +225,8 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 			myHttpVersionBox.addItem(httpVersion);
 		}
 
-		myResponseSplitter.setProportion(0.2f);
-		myResponseSplitter.setSplitterProportionKey("RestClientPanel#response");
+		responseSplitter.setProportion(0.2f);
+		responseSplitter.setSplitterProportionKey("RestClientPanel#response");
 
 		ListTableModel<Pair<String, String>> model = new ListTableModel<Pair<String, String>>(new ColumnInfo[]{
 				new ColumnInfo<Pair<String, String>, String>("HTTP Header")
@@ -189,7 +253,7 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 		final JBTable table = new JBTable(model);
 		table.setShowColumns(true);
 
-		myResponseSplitter.setFirstComponent(new JBScrollPane(table));
+		responseSplitter.setFirstComponent(ScrollPaneFactory.createScrollPane(table, SideBorder.LEFT | SideBorder.BOTTOM));
 
 		myEditorTextField = new EditorTextField(EditorFactory.getInstance().createDocument(""), project, PlainTextFileType.INSTANCE, true, false)
 		{
@@ -203,7 +267,7 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 		};
 		myEditorTextField.setFontInheritedFromLAF(false);
 
-		myResponseSplitter.setSecondComponent(new JBScrollPane(myEditorTextField));
+		responseSplitter.setSecondComponent(ScrollPaneFactory.createScrollPane(myEditorTextField, SideBorder.RIGHT));
 
 		myUrlTextField.addDocumentListener(new DocumentAdapter()
 		{
@@ -307,8 +371,7 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 											}
 
 											EditorFactoryImpl editorFactory = (EditorFactoryImpl) EditorFactory.getInstance();
-											Document document = editorFactory.createDocument(body.string(), true,
-													true);
+											Document document = editorFactory.createDocument(body.string(), true, true);
 
 											myEditorTextField.setNewDocumentAndFileType(fileType, document);
 
@@ -319,7 +382,7 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 												@Override
 												public void run()
 												{
-													myTabPanel.setSelectedComponent(myResponseTab);
+													tabbedPaneWrapper.setSelectedComponent(myResponseTab);
 												}
 											});
 										}
@@ -334,8 +397,7 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 
 		AnAction restClientToolbarActions = ActionManager.getInstance().getAction("RESTClientToolbarActions");
 
-		ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, (ActionGroup) restClientToolbarActions,
-				false);
+		ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, (ActionGroup) restClientToolbarActions, false);
 
 		myRootPanel.add(actionToolbar.getComponent(), BorderLayout.WEST);
 	}
@@ -370,31 +432,6 @@ public class RestClientPanel extends Ref<Project>  implements Disposable
 	public JPanel getRootPanel()
 	{
 		return myRootPanel;
-	}
-
-	private void createUIComponents()
-	{
-		myUrlTextField = new TextFieldWithAutoCompletionWithEnter(get(), new TextFieldWithAutoCompletion.StringsCompletionProvider(null, null)
-		{
-			@NotNull
-			@Override
-			public Collection<String> getItems(String prefix, boolean cached, CompletionParameters parameters)
-			{
-				if(prefix == null)
-				{
-					return Collections.emptyList();
-				}
-
-				List<String> list = new ArrayList<String>();
-				for(RequestBean o : RestClientHistoryManager.getInstance(get()).getRequests().values())
-				{
-					list.add(o.getUrl().toString());
-				}
-				return list;
-			}
-		}, false, null);
-		myUrlTextField.setPlaceholder("URL");
-
 	}
 
 	@Override
