c5e73fe357032b46f331c23dfc5a3256ccf7a01e
VISTALL <vistall.valeriy@gmail.com>
2021-12-25 12:40:01 +0300

    update ui - add borders

--- src/main/java/consulo/restclient/ui/RestClientPanel.java
+++ src/main/java/consulo/restclient/ui/RestClientPanel.java
@@ -37,10 +37,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.ColoredListCellRenderer;
-import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.ui.TabbedPaneWrapper;
-import com.intellij.ui.TextFieldWithAutoCompletion;
+import com.intellij.ui.*;
+import com.intellij.ui.components.panels.Wrapper;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.util.concurrency.AppExecutorUtil;
@@ -50,6 +48,7 @@ import consulo.disposer.Disposable;
 import consulo.restclient.HttpHeader;
 import consulo.restclient.RestClientHistoryManager;
 import consulo.ui.annotation.RequiredUIAccess;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.apache.hc.client5.http.async.methods.SimpleBody;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
@@ -64,8 +63,6 @@ import org.wiztools.restclient.bean.RequestBean;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import jakarta.inject.Inject;
-
 import javax.swing.*;
 import java.awt.*;
 import java.net.MalformedURLException;
@@ -129,7 +126,10 @@ public class RestClientPanel implements Disposable
 		panel2.setLayout(new GridLayoutManager(1, 3, JBUI.emptyInsets(), -1, -1));
 		panel1.add(panel2, BorderLayout.NORTH);
 		myMethodComboBox = new JComboBox<>();
-		panel2.add(myMethodComboBox, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints
+		Wrapper comboWrapper = new Wrapper(myMethodComboBox);
+		comboWrapper.setBorder(JBUI.Borders.empty(0, 9, 0, 0));
+
+		panel2.add(comboWrapper, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints
 				.SIZEPOLICY_FIXED, null, null, null, 0, false));
 		myUrlTextField = new TextFieldWithAutoCompletionWithEnter(myProject, new TextFieldWithAutoCompletion.StringsCompletionProvider(null, null)
 		{
@@ -379,7 +379,8 @@ public class RestClientPanel implements Disposable
 		AnAction restClientToolbarActions = ActionManager.getInstance().getAction("RESTClientToolbarActions");
 
 		ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, (ActionGroup) restClientToolbarActions, false);
-
+		actionToolbar.setTargetComponent(myRootPanel);
+		actionToolbar.getComponent().setBorder(JBUI.Borders.customLine(JBColor.border(), 0, 0, 0, 1));
 		myRootPanel.add(actionToolbar.getComponent(), BorderLayout.WEST);
 	}
 
